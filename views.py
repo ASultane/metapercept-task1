0995e139705053d87fbd8fa0d3b57256b88c679b
Achal Sultane <149863174+ASultane@users.noreply.github.com>
2024-08-21 16:37:52 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Course
+from .forms import CourseForm
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'course_list.html', {'courses': courses})
+
+def course_detail(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    return render(request, 'course_detail.html', {'course': course})
+
+def course_create(request):
+    if request.method == "POST":
+        form = CourseForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('course_list')
+    else:
+        form = CourseForm()
+    return render(request, 'course_form.html', {'form': form})
+
+def course_update(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    if request.method == "POST":
+        form = CourseForm(request.POST, request.FILES, instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect('course_list')
+    else:
+        form = CourseForm(instance=course)
+    return render(request, 'course_form.html', {'form': form})
+
+def course_delete(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    if request.method == "POST":
+        course.delete()
+        return redirect('course_list')
+    return render(request, 'course_confirm_delete.html', {'course': course})
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def blog(request):
+    return render(request, 'blog.html')
